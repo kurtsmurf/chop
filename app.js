92ac4bbfd39f9f21b5123d8492795ded7e1ab72a
Chris Smith <chrisronsmith@gmail.com>
2020-08-15 11:57:00 -0400

    Convert files to audioBuffers

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
 import { h, render } from 'https://unpkg.com/preact@latest?module'
 import { useState } from 'https://unpkg.com/preact@latest/hooks/dist/hooks.module.js?module'
 
-const ChooseFile = ({addFile}) => (
+const audioContext = new AudioContext()
+
+const ChooseFile = ({ takeFile }) => (
   h(
     'input',
     {
       type: 'file',
       accept: 'audio/*',
-      onChange: e => addFile(e.target.files[0])
+      onChange: e => takeFile(e.target.files[0])
     }
   )
 )
 
+const fileToAudioBuffer = (file, takeAudioBuffer) => {
+  const reader = new FileReader()
+
+  reader.onloadend = async e => {
+    const arrayBuffer = event.target.result
+    const audioBuffer = await audioContext.decodeAudioData(arrayBuffer)
+
+    takeAudioBuffer(audioBuffer)
+  }
+
+  reader.readAsArrayBuffer(file)
+}
+
 const App = () => {
-  const [files, setFiles] = useState([])
+  const [audioBuffer, setAudioBuffer] = useState(undefined)
 
-  const addFile = file => setFiles([...files, file])
+  console.log(audioBuffer)
 
-  return h(ChooseFile, { addFile })
+  const takeFile = file => {
+    fileToAudioBuffer(file, audioBuffer => {
+      setAudioBuffer(audioBuffer)
+    })
+  }
+
+  return h(ChooseFile, { takeFile })
 }
 
 render(
